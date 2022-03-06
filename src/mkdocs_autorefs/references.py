5cb12a70427d0f70c7b6966d147d2eed11f0a2d7
Timothée Mazzucotelli <pawamoy@pm.me>
2022-03-06 17:49:08 +0100

    Update src/mkdocs_autorefs/references.py
    Co-authored-by: Oleh Prypin <oleh@pryp.in>

--- src/mkdocs_autorefs/references.py
+++ src/mkdocs_autorefs/references.py
@@ -165,8 +165,8 @@ def fix_ref(url_mapper: Callable[[str], str], unmapped: List[str]) -> Callable: 
                 return f"[{identifier}][]"
             return f"[{title}][{identifier}]"
 
-        classes = "autorefs "
-        classes += "external" if urlparse(url).scheme else "internal"
+        classes = "autorefs"
+        classes += " external" if urlparse(url).scheme else " internal"
         if kind == "autorefs-optional-hover":
             return f'<a class="{classes}" title="{identifier}" href="{escape(url)}">{title}</a>'
         return f'<a class="{classes}" href="{escape(url)}">{title}</a>'
