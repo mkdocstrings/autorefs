72e99685f2c77e6c7290f71c82926c5e50d1221d
Markus Beil <mb@beil-dev.de>
2024-07-04 22:35:04 +0200

    warning for multiple same anchors

--- src/mkdocs_autorefs/plugin.py
+++ src/mkdocs_autorefs/plugin.py
@@ -57,7 +57,7 @@ class AutorefsPlugin(BasePlugin):
     def __init__(self) -> None:
         """Initialize the object."""
         super().__init__()
-        self._url_map: dict[str, str] = {}
+        self._url_map: dict[str, list[str]] = {}
         self._abs_url_map: dict[str, str] = {}
         self.get_fallback_anchor: Callable[[str], tuple[str, ...]] | None = None
 
@@ -68,7 +68,12 @@ class AutorefsPlugin(BasePlugin):
             page: The relative URL of the current page. Examples: `'foo/bar/'`, `'foo/index.html'`
             identifier: The HTML anchor (without '#') as a string.
         """
-        self._url_map[identifier] = f"{page}#{anchor or identifier}"
+        page_anchor = f"{page}#{anchor or identifier}"
+        if identifier in self._url_map:
+            if page_anchor not in self._url_map[identifier]:
+                self._url_map[identifier].append(page_anchor)
+        else:
+            self._url_map[identifier] = [page_anchor]
 
     def register_url(self, identifier: str, url: str) -> None:
         """Register that the identifier should be turned into a link to this URL.
@@ -85,7 +90,11 @@ class AutorefsPlugin(BasePlugin):
         fallback: Callable[[str], Sequence[str]] | None = None,
     ) -> str:
         try:
-            return self._url_map[identifier]
+            if len(self._url_map[identifier]) > 1:
+                log.warning(
+                    f"Multiple urls found for '{identifier}': {self._url_map[identifier]}. Please specify a custom unique identifier.",
+                )
+            return self._url_map[identifier][0]
         except KeyError:
             if identifier in self._abs_url_map:
                 return self._abs_url_map[identifier]
@@ -94,7 +103,7 @@ class AutorefsPlugin(BasePlugin):
                 for new_identifier in new_identifiers:
                     with contextlib.suppress(KeyError):
                         url = self._get_item_url(new_identifier)
-                        self._url_map[identifier] = url
+                        self._url_map[identifier] = [url]
                         return url
             raise
 
--- tests/test_references.py
+++ tests/test_references.py
@@ -113,7 +113,7 @@ def test_reference_implicit_with_code_inlinehilite_python() -> None:
         extensions={"pymdownx.inlinehilite": {"style_plain_text": "python"}, "pymdownx.highlight": {}},
         url_map={"pathlib.Path": "pathlib.html#Path"},
         source="This [`pathlib.Path`][].",
-        output='<p>This <a class="autorefs autorefs-internal" href="pathlib.html#Path"><code class="highlight">pathlib.Path</code></a>.</p>',
+        output='<p>This <a class="autorefs autorefs-internal" href="pathlib.html#Path"><code class="language-python highlight">pathlib.Path</code></a>.</p>',
     )
 
 
@@ -326,22 +326,28 @@ def test_register_markdown_anchors() -> None:
             ## Heading more2 {#heading-custom2}
 
             [](){#alias10}
+
+            [](){#aliasSame}
+            ## Same heading 1
+            [](){#aliasSame}
+            ## Same heading 2
             """,
         ),
     )
     assert plugin._url_map == {
-        "foo": "page#heading-foo",
-        "bar": "page#bar",
-        "alias1": "page#heading-bar",
-        "alias2": "page#heading-bar",
-        "alias3": "page#alias3",
-        "alias4": "page#heading-baz",
-        "alias5": "page#alias5",
-        "alias6": "page#alias6",
-        "alias7": "page#alias7",
-        "alias8": "page#alias8",
-        "alias9": "page#heading-custom2",
-        "alias10": "page#alias10",
+        "foo": ["page#heading-foo"],
+        "bar": ["page#bar"],
+        "alias1": ["page#heading-bar"],
+        "alias2": ["page#heading-bar"],
+        "alias3": ["page#alias3"],
+        "alias4": ["page#heading-baz"],
+        "alias5": ["page#alias5"],
+        "alias6": ["page#alias6"],
+        "alias7": ["page#alias7"],
+        "alias8": ["page#alias8"],
+        "alias9": ["page#heading-custom2"],
+        "alias10": ["page#alias10"],
+        "aliasSame": ["page#same-heading-1", "page#same-heading-2"]
     }
 
 
@@ -366,9 +372,9 @@ def test_register_markdown_anchors_with_admonition() -> None:
         ),
     )
     assert plugin._url_map == {
-        "alias1": "page#alias1",
-        "alias2": "page#heading-bar",
-        "alias3": "page#alias3",
+        "alias1": ["page#alias1"],
+        "alias2": ["page#heading-bar"],
+        "alias3": ["page#alias3"],
     }
 
 
